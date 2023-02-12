ea410a405c961f042b98d86c5b4c4419acd2c9f3
juankmanca <juankmanca@gmail.com>
2023-02-11 20:56:03 -0500

    connection local database

---
+++ Sales.Share/entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Share.entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = " El campo {0} es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caractéres ")]
+        [Display(Name = "País")]
+        public string Name { get; set; } = null!;
+    }
+}
