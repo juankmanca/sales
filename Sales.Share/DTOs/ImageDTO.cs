ca52fc94d55b2063557cca2cae516f5eeb733741
juankmanca <juankmanca@gmail.com>
2023-03-30 18:33:29 -0500

    Añadir imagenes

---
+++ Sales.Share/DTOs/ImageDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Share.DTOs
+{
+    public class ImageDTO
+    {
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public List<string> Images { get; set; } = null!;
+    }
+}
