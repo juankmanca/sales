fa0aa941a1da20e21d4a2e78525f9e9027145586
juankmanca <juankmanca@gmail.com>
2023-03-30 19:22:13 -0500

    Carrito de compras

---
+++ Sales.Share/DTOs/SaleDTO.cs
@@ -0,0 +1,13 @@
+﻿using Sales.Share.Enums;
+
+namespace Sales.Share.DTOs
+{
+    public class SaleDTO
+    {
+        public int Id { get; set; }
+
+        public OrderStatus OrderStatus { get; set; }
+
+        public string Remarks { get; set; } = string.Empty;
+    }
+}
---
+++ Sales.Share/Enums/OrderStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace Sales.Share.Enums
+{
+    public enum OrderStatus
+    {
+        Nuevo,
+        Despachado,
+        Enviado,
+        Confirmado,
+        Cancelado
+    }
+}
+
